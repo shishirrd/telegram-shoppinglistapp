37d39c10baa9fd4a7251b5c4b9cb70c405a58035
shishirrd <88389278+shishirrd@users.noreply.github.com>
2021-10-17 10:20:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,61 +1,26 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import requests
 import json
 import telebot
 import sys
 
-
-# In[ ]:
-
+import os
+PORT = int(os.environ.get('PORT',5000))
 
 API_KEY = '2077563577:AAFruWz-P4oxbCHFPC1ptUlvjMjaYausaZw'
 
-
-# In[ ]:
-
-
 bot = telebot.TeleBot(API_KEY)
-
-
-# In[ ]:
-
 
 #Define initial shopping list
 shopping_list = ['Apples','Bananas','Pears','Carrots','Potatoes']
 
-
-# In[ ]:
-
-
 import telegram.ext
-
-
-# In[ ]:
-
 
 updater = telegram.ext.Updater(API_KEY, use_context=True)
 
-
-# In[ ]:
-
-
 disp = updater.dispatcher
-
-
-# In[ ]:
-
 
 def hello(update, context):
     update.message.reply_text("Hello folks! Welcome to Shishir's shopping list bot!. Click on /start to get started!")
-
-
-# In[ ]:
-
 
 #Define main menu for user to see when they first access the bot
 def start(update, context):
@@ -70,15 +35,9 @@ def start(update, context):
 6. Type /Clear to clear shopping list""")
 
 
-# In[ ]:
-
-
 def viewList(update, context):
     update.message.reply_text("Here's your list!") 
     update.message.reply_text(str(shopping_list))
-
-
-# In[ ]:
 
 
 def addList(update, context):
@@ -87,16 +46,10 @@ def addList(update, context):
     update.message.reply_text(f"{item} has been added to the list!")
 
 
-# In[ ]:
-
-
 def removeList(update, context):
     item = context.args[0]
     shopping_list.remove(item)
     update.message.reply_text(f"{item} has been removed from the list!")
-
-
-# In[ ]:
 
 
 #Define function for checking whether item is on list
@@ -108,9 +61,6 @@ def checkItem(update, context):
         update.message.reply_text(f"No. {item} is not on the list! Would you like to add this?")
 
 
-# In[ ]:
-
-
 #Define function for counting no. of items on list
 def listLength(update, context):
     count = len(shopping_list)
@@ -120,17 +70,10 @@ def listLength(update, context):
         update.message.reply_text("There are no items on the shopping list!")
 
 
-# In[ ]:
-
-
 #Define function to clear shopping list and start over afresh
 def clearList(update, context):
     shopping_list.clear()
     update.message.reply_text("Shopping list is now empty!")
-
-
-# In[ ]:
-
 
 disp.add_handler(telegram.ext.CommandHandler("hello", hello))
 disp.add_handler(telegram.ext.CommandHandler("start", start))
@@ -141,10 +84,8 @@ disp.add_handler(telegram.ext.CommandHandler("Check", checkItem))
 disp.add_handler(telegram.ext.CommandHandler("Count", listLength))
 disp.add_handler(telegram.ext.CommandHandler("Clear", clearList))
 
-
-# In[ ]:
-
-
-updater.start_polling()
+updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=API_KEY)
+updater.bot.setWebhook('https://shishir-telegram-shoppinglist.herokuapp.com/' + API_KEY)
 updater.idle()
-
